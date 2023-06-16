fb5a86f455643c7049678da48dd45f9b0e7135ee
TruongTheHieu <Thehieu191099@gmail.com>
2023-06-16 11:25:49 +0700

    thay doi

---
+++ ss7_bien_va_kieu_du_lieu/thực hành/sử dụng toán từ.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputHeight;
+inputwidth = prompt("nhapchieurong");
+inputHeight = prompt(" nhapchieucao");
+let Width= parseInt(inputwidth);
+let Height = parseInt(inputHeight)
+let area = Width*Height;
+document.write("diện tích hình chữ nhật là"+ area);
